7162d0ad7a5314cf9e7dc7cf04402fb6b22b7557
Carter Morgan <askcarter@google.com>
2016-05-18 00:15:25 -0700

    Purged extra files from repo.

--- craft/cleanup.sh
+++
@@ -1,3 +0,0 @@
-gcloud compute instances delete node0 node1
-gcloud compute routes delete default-route-10-200-1-0-24 default-route-10-200-0-0-24
-gcloud compute firewall-rules delete default-allow-local-api
