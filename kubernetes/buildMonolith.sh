ee178485a1e1f1bce6197c72f10ec9594ecfc08c
Charles Stockman <chuck.stockman@yahoo.com>
2022-01-26 22:34:24 -0500

    Worked more on kubernetes

--- kubernetes/buildMonolith.sh
+++ kubernetes/buildMonolith.sh
@@ -3,17 +3,21 @@
 #
 
 # Create the Monolith pod using the udacity/example-monolith:1.0.0 
-kubectl create -f pods/monolith.yaml
+# This takes a very time, but eventually it will succeed need to fix
+kubectl create -f pods/monolith.yaml 
+
+# Wait until the creation has been completed
+kubectl wait --for=condition=ready --timeout=30s pod/monolith
 
 # Relate a private ip address ( of the pod ) with a public address
-kubectl port-forward monolith 10000:80
+kubectl port-forward monolith 10080:80 &
 
 
 
 # Get Basic information about the pods
-echo "************************** Basic Information"
-kubectl get pods
+#echo "************************** Basic Information"
+#kubectl get pods
 
 # Get more information about the pods
-echo "************************** Detailed Information"
-kubectl describe pods monolith
+#echo "************************** Detailed Information"
+#kubectl describe pods monolith
--- kubernetes/useMonolith.sh
+++ kubernetes/useMonolith.sh
@@ -7,3 +7,18 @@ echo "************************"
 echo "Show the Authentication Service"
 curl http://localhost:10080/secure
 echo "************************"
+
+echo "Get the login token for the person with the username user"
+token=`curl -u user:password http://localhost:10080/login | jq .token | tr -d \"`
+echo $token
+echo "*************************************************"
+
+echo "Login with the token"
+curl -H "Authorization: Bearer $token" http://localhost:10080/secure
+echo "*************************************************"
+
+#
+# Can use the -f option kubectl logs -f monolith to see the logs in real time
+echo "Show Logs"
+kubectl logs monolith
+echo "************************":
